c1ca32b95cce841d8b09fc4d0bfb0d240a82117f
methokb <abdurasulovrahmatso@gmail.com>
2023-01-09 15:19:05 +0300

    dz p 1 js

---
+++ main.js
@@ -0,0 +1,28 @@
+// let i = 0;
+// function consoleRec(arr){
+//     console.log(arr[i]);
+//     i++;
+//     if (i < arr.length) consoleRec(arr);
+// }
+// console.log(consoleRec([1,2,3]), "array")
+
+
+// let arr = [4,5,6]
+// arr.unshift(1,2,3)
+// arr.push(7,8,9,10)
+// console.log(arr);
+
+
+// let arr = [1,2,3,4,5]
+
+// function killer (arr){
+//     return arr [arr.length - 1]
+// }
+// console.log(killer (arr));
+
+
+// let text = 'loremipsumdolor'
+// let regex = /o/g;
+// console.log(text.match(regex));
+
+
